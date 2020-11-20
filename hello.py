b0510aeda68841d9d76d53fe5988ab2c6fffc0d4
Mingqing Sun <msun13@u.rochester.edu>
2020-11-21 01:22:12 +0800

    1	modified:   .DS_Store 1	new file:   community.db 1	modified:   hello.py 1	new file:   requirements.txt 1	modified:   static/.DS_Store 1	modified:   static/css/style.css 1	modified:   static/styles.css 1	new file:   templates/account.html 1	modified:   templates/base.html 1	modified:   templates/index.html 1	new file:   templates/login.html 1	new file:   templates/register.html

--- hello.py
+++ hello.py
@@ -1,35 +1,71 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,flash,url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField,PasswordField,BooleanField
+from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, current_user,logout_user, login_required
 
 app = Flask(__name__)
 application=app
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///ingred.db'
-
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///community.db'
+bcrypt = Bcrypt(app)
 db=SQLAlchemy(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category = 'info'
 
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(30),unique=True, nullable=False)
+    email=db.Column(db.String(120),unique=True, nullable=False)
+    password=db.Column(db.String(60), nullable=False)
+    posts=db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}',{self.email}')"
 
+class Post(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(120), nullable=False)
+    content=db.Column(db.Text, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title})"
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3,max=30)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists.')
+    def validate_email(self, email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists.')
+        
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class ingred(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String(200),nullable=False)
-
-
-class IngredForm(FlaskForm):
-    item = StringField('Add a new ingredient:')
-    submit = SubmitField('Submit')
-
-def __repr__(self):
-    return '<Ingredient %r>' % self.id
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -41,43 +77,51 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    item = None
-    form = IngredForm()
-    if request.method == "POST":
-        item_name = request.form['item']
-        new_item = ingred(item=item_name)
-        try:
-            db.session.add(new_item)
-            db.session.commit()
-            return redirect('/crud')
-        except:
-            return "There's something wrong!"
-    else:
-        ing=ingred.query.order_by(ingred.id)
-        return render_template('index.html', form=form, ing=ing)
+@app.route('/')
 
-@app.route('/update/<int:id>',methods=['GET', 'POST'])
-def update(id):
-    ingred_to_update = ingred.query.get_or_404(id)
-    if request.method=="POST":
-        ingred_to_update.item = request.form['item']
-        try:
-            db.session.commit()
-            return redirect('/crud')
-        except:
-            return "Something went wrong..."
-    else:
-        return render_template('update.html', ingred_to_update=ingred_to_update)
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    friend_to_delete = ingred.query.get_or_404(id)
-    try:
-        db.session.delete(friend_to_delete)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
         db.session.commit()
-        return redirect('/crud')
-    except:
-        return "Something went wrong..."
+        flash('Welcome to our community! You can now login.','success')
+        return redirect(url_for('login'))
+    return render_template('register.html',form=form)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash(f'Login succeeded. Welcome! { user.username } .','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed, please check your email and password.','danger')
+    return render_template('login.html',form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+     return render_template('account.html',title='Account')
+
 
